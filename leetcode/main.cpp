40b7666702178277b975ae3e96cf3f07d10cff54
shane <enginzhu@gmail.com>
2016-03-03 22:50:55 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by 朱晓杨 on 16/3/3.
+//  Copyright © 2016年 shane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
